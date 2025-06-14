66bd9580512cc0636ffa04dbf2889b4c7f503fe7
yashodhah <y.dominatz@gmail.com>
2025-06-14 23:22:49 +0800

    tea-mates | aws-plumbing

--- .deployments/aws/vpc.tf
+++ .deployments/aws/vpc.tf
@@ -6,10 +6,10 @@ module "vpc" {
   cidr = local.vpc_cidr
 
   azs              = ["ap-southeast-1a", "ap-southeast-1b", "ap-southeast-1c"]
-  public_subnets   = ["10.16.48.0/20", "10.16.112.0/20", "10.16.176.0/20"] # Web tier
-  private_subnets  = ["10.16.16.0/20", "10.16.80.0/20", "10.16.144.0/20"]  # App tier
-  database_subnets = ["10.16.32.0/20", "10.16.96.0/20", "10.16.160.0/20"]  # DB tier
-  intra_subnets    = ["10.16.0.0/20", "10.16.64.0/20", "10.16.128.0/20"]   # Reserved tier
+  public_subnets   = ["10.16.0.0/20", "10.16.16.0/20", "10.16.32.0/20"]   # Web tier
+  private_subnets  = ["10.16.48.0/20", "10.16.64.0/20", "10.16.80.0/20"]  # App tier
+  database_subnets = ["10.16.96.0/20", "10.16.112.0/20", "10.16.128.0/20"]# DB tier
+  intra_subnets    = ["10.16.144.0/20", "10.16.160.0/20", "10.16.176.0/20"]# Reserved
 
   create_database_subnet_group  = true
   manage_default_network_acl    = false
