dd01af80cc85157082f797200c8121e5ce47206a
yashodhah <y.dominatz@gmail.com>
2025-06-22 23:02:11 +0800

    tea-mates | aws-plumbing

--- .deployments/aws/versions.tf
+++ .deployments/aws/versions.tf
@@ -8,11 +8,11 @@ terraform {
     }
   }
 
-  # backend "s3" {
-  #   bucket = "dev.labs.yashodha.terraform"
-  #   key    = "dev"
-  #   region = "ap-southeast-1"
-  # }
+  backend "s3" {
+    bucket = "dev.labs.yashodha.terraform"
+    key    = "dev"
+    region = "ap-southeast-1"
+  }
 }
 
 
